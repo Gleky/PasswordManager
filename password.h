c5bc71095886e8add1dd9b85005b036f0348aa94
Gleb K <glebgkom@gmail.com>
2021-03-08 20:04:24 +0300

    Password struct created

---
+++ password.h
@@ -0,0 +1,14 @@
+#ifndef PASSWORD_H
+#define PASSWORD_H
+
+#include <QString>
+
+struct Password
+{
+    QString title;
+    QString description;
+    QString login;
+    QString password;
+};
+
+#endif // PASSWORD_H
