a5200931cf9ba6cd216e894a6a09be27caba0fd3
Gleb K <glebgkom@gmail.com>
2021-03-20 21:51:40 +0300

    Inherited QObject for registering as interface

--- istorage.h
+++ istorage.h
@@ -2,10 +2,13 @@
 #define ISTORAGE_H
 
 #include <QList>
+#include <QObject>
+
 struct Password;
 
-class IStorage
+class IStorage : public QObject
 {
+    Q_OBJECT
 public:
     virtual void store(const QList<Password> &passwords) const = 0;
     virtual void load(QList<Password> &passwords) = 0;
